f4363c261c52cd4225864c0c5b9577930f91f4b4
huanniangstudio <891863891@qq.com>
2022-05-27 16:17:35 +0800

    完善导入私钥功能

--- src/popup/pages/Account/Import.tsx
+++ src/popup/pages/Account/Import.tsx
@@ -1,4 +1,6 @@
+import { useWallet } from '@/ui/utils'
 import { Button, Input } from 'antd'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Status } from '.'
 
@@ -8,10 +10,17 @@ interface Props {
 
 export default ({ setStatus }: Props) => {
   const navigate = useNavigate()
-
-  const verify = () => {
-    // to verify
-    navigate('/create-recovery')
+  const wallet = useWallet()
+  const [privateKey, setPrivateKey] = useState('')
+  const [inputError, setInputError] = useState('')
+  const verify = async () => {
+    try {
+      const _res = await wallet.importPrivateKey(privateKey)
+      await wallet.changeAccount(_res[0])
+      navigate('/dashboard')
+    } catch (e) {
+      setInputError((e as any).message)
+    }
   }
 
   return (
@@ -19,16 +28,24 @@ export default ({ setStatus }: Props) => {
       <div className="flex items-center px-2 text-2xl h-13">Import Private Key</div>
       <div className="text-base text-soft-white">Imported accounts will not be associated with your originally created Paragon account Secret Recovery Phrase.</div>
       <div className="flex items-center w-full p-5 mt-1_25 h-15_5 box default">
-        <Input className="font-semibold text-white p0" bordered={false} status="error" placeholder="Private Key" />
+        <Input
+          className="font-semibold text-white p0"
+          bordered={false}
+          status="error"
+          placeholder="Private Key"
+          onChange={(e) => {
+            setPrivateKey(e.target.value)
+          }}
+        />
       </div>
+      <div className="text-base text-soft-white">{inputError}</div>
       <Button
         size="large"
         type="primary"
         className="box w380"
         onClick={(e) => {
           verify()
-        }}
-      >
+        }}>
         <div className="flex items-center justify-center text-lg">Import Private Key</div>
       </Button>
     </div>
